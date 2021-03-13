839792d1fa2d11e284eda65eb80d932ceea8442d
mosesbenjamin <mavewreek@gmail.com>
2021-03-13 01:46:58 +0100

    restoring spies

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -1,13 +1,17 @@
 const { expect } = require('chai')
-const { createSandbox, spy } = require('sinon')
+const sinon = require('sinon')
 const fs = require('fs')
 const proxyquire = require('proxyquire')
 const fileManagement = require('./file.management')
 
 describe('File Management', () => {
+  afterEach(() => {
+    sinon.restore()
+  })
+
   describe('when creating a new file', () => {
     it('should call writeFileSync', () => {
-      const writeSpy = spy(fs, 'writeFileSync')
+      const writeSpy = sinon.spy(fs, 'writeFileSync')
       const fileManagement = proxyquire('./file.management', { fs })
       fileManagement.createFile('test.txt')
 
@@ -15,12 +19,13 @@ describe('File Management', () => {
     })
 
     it('should not create a new file if no filename is specified', () => {
-      const writeSpy = spy(fs, 'writeFileSync')
+      const writeSpy = sinon.spy(fs, 'writeFileSync')
       const fileManagement = proxyquire('./file.management', { fs })
 
       try {
         fileManagement.createFile()
       } catch (error) {}
+
       expect(writeSpy.notCalled).to.be.true
     })
 
