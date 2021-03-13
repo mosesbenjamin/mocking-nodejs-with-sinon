25c648c96820e364b4c5abdc56e66cc802a481f6
mosesbenjamin <mavewreek@gmail.com>
2021-03-13 01:29:13 +0100

    Creating a spy

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -1,11 +1,16 @@
-const { expect } = require("chai");
-const { createSandbox, spy } = require("sinon");
-const fs = require("fs");
-const proxyquire = require("proxyquire");
-const fileManagement = require("./file.management");
+const { expect } = require('chai')
+const { createSandbox, spy } = require('sinon')
+const fs = require('fs')
+const proxyquire = require('proxyquire')
+const fileManagement = require('./file.management')
 
-describe("File Management", () => {
-  it("true should be true", () => {
-    expect(true).to.be.true;
-  });
-});
+describe('File Management', () => {
+  describe('when creating a new file', () => {
+    it('should call writeFileSync', () => {
+      const writeSpy = spy(fs, 'writeFileSync')
+      fileManagement.createFile('test.txt')
+
+      expect(writeSpy.calledWith('./data/test.txt', '')).to.be.true
+    })
+  })
+})
