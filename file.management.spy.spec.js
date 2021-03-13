d77568db23c01e5ce9ead96716ce5cfdd138d7ec
mosesbenjamin <mavewreek@gmail.com>
2021-03-13 01:40:09 +0100

    using a spy

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -8,7 +8,25 @@ describe('File Management', () => {
   describe('when creating a new file', () => {
     it('should call writeFileSync', () => {
       const writeSpy = spy(fs, 'writeFileSync')
+      const fileManagement = proxyquire('./file.management', { fs })
       fileManagement.createFile('test.txt')
+
+      expect(writeSpy.calledWith('./data/test.txt', '')).to.be.true
+    })
+
+    it('should not create a new file if no filename is specified', () => {
+      const writeSpy = spy(fs, 'writeFileSync')
+      const fileManagement = proxyquire('./file.management', { fs })
+
+      try {
+        fileManagement.createFile()
+      } catch (error) {}
+      expect(writeSpy.notCalled).to.be.true
+    })
+
+    it.skip('should call writeFileSync - injected', () => {
+      const writeSpy = spy(fs, 'writeFileSync')
+      fileManagement.createFileInjected('test.txt', fs)
 
       expect(writeSpy.calledWith('./data/test.txt', '')).to.be.true
     })
