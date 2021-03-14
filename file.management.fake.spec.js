22a1edefdbcbfe4659fea64fb5321b8eb38a0868
mosesbenjamin <mavewreek@gmail.com>
2021-03-14 15:15:48 +0100

    replacing stubs with fakes

--- file.management.fake.spec.js
+++ file.management.fake.spec.js
@@ -17,4 +17,25 @@ describe('File Management', () => {
     fileManagement.createFile('test.txt')
     expect(writeFake.calledWith('./data/test.txt')).to.be.true
   })
+
+  it('Should throw an exception when the file already exists', () => {
+    const writeFake = sinon.fake.throws(new Error())
+    sinon.replace(fs, 'writeFileSync', writeFake)
+
+    const fileManagement = proxyquire('./file.management', { fs })
+
+    expect(() => fileManagement.createFile('test.txt')).to.throws()
+  })
+
+  it('getAllFiles should return a list of files', () => {
+    const readFake = sinon.fake.yields(null, ['test.txt'])
+
+    sinon.replace(fs, 'readdir', readFake)
+
+    const fileManagement = proxyquire('./file.management', { fs })
+
+    fileManagement.getAllFiles((err, data) => {
+      expect(data).to.eql(['test.txt'])
+    })
+  })
 })
